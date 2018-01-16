05a9a637fc708a19e64f6c147655d0ee67abd4bc
Chris Walker <crwalker4@yahoo.co.uk>
2018-01-16 21:34:10 +0000

    JS for datepickers - TODO: move to generic helper

--- Watermark/src/js/pages/products_new.js
+++ Watermark/src/js/pages/products_new.js
@@ -18,17 +18,35 @@ var onFailed = function (context) {
     alert("Failed");
 }; 
 
-// Setup logic
+// Start
 $(document).ready(function () {
-    $("#datepicker-from").datepicker();
-    $("#datepicker-to").datepicker();
+    datePickerInit();
+    specialPriceInit();
+    clearLabelInit();
 });
 
+// Setup logic for datepickers
+var datePickerInit = function () {
+    $("#datepicker-from").datepicker();
+    $("#datepicker-to").datepicker();
+};
+
 // Logic to collapse / expand the Special Price panel
-$(document).ready(function () {
+var specialPriceInit = function () {
     $('#special-price-display-body').hide();
 
     $('#special-price-display').change(function () {
         $('#special-price-display-body').slideToggle("fast");
     });
-});
+};
+
+// Logic to control the 'clear field label' -- TODO Move to tag helper
+var clearLabelInit = function () {
+    $('label[data-for="datepicker-from"]').on('click', function () {
+        $('#datepicker-from').val('');
+    });
+
+    $('label[data-for="datepicker-to"]').on('click', function () {
+        $('#datepicker-to').val('');
+    });
+};
